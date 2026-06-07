528c8bb607eb5f6a55686fef693c7f9bd69658c1
zoka <zokaxxdmenendes@gmail.com>
2026-06-07 19:17:14 -0300

    hash, token

--- main.py
+++ main.py
@@ -9,49 +9,91 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from dotenv import load_dotenv
 import os
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
-
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-#--------models------
+#================= Hash
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password[:72])
+
+def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password[:72], hashed_password)
+
+
+# ============ Token func
+
+def create_token(data: dict):
+    dados = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    dados.update({"exp": expire})
+    token = jwt.encode(dados, SECRET_KEY, algorithm=ALGORITHM)
+    return token
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        email = payload.get("email")
+
+        if user_id is None or email is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        
+        return {"user_id": user_id, "email": email}
+    
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido")
+
+
+#============== Models
 class User(BaseModel):
     name: str
     age: int
     email: EmailStr
     password: str
 
-class post(BaseModel):
+class Post(BaseModel):
     title: str
     content: str
 
-class coment(BaseModel):
+class Comment(BaseModel):
     content: str
 
-#--------tables------
+#============== Tables
 
 def create_table():
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
     ''')
     conn.commit()
     conn.close()
 create_table()
 
 def create_post_table():
-    conn = sqlite3.connect('posts.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS posts (
@@ -67,7 +109,7 @@ def create_post_table():
 create_post_table()
 
 def create_comment_table():
-    conn = sqlite3.connect('comments.db')
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS comments (
